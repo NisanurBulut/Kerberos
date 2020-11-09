e76d0d4bf46a1ef7280d55d7e4f27908b8c10bac
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-09 21:23:19 +0300

    manager classes are implemented

---
+++ Kerberos.Business/Concrete/AppRoleManager.cs
@@ -0,0 +1,17 @@
+﻿using Kerberos.Business.Interfaces;
+using Kerberos.DataAccess.Interfaces;
+using Kerberos.Entities.Concrete;
+using Kerberos.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+namespace Kerberos.Business.Concrete
+{
+    public class AppRoleManager<TEntity> : BaseManager<AppRole>, IAppRoleService
+    {
+        public AppRoleManager(IBaseRepository<AppRole> baseRepo) : base(baseRepo)
+        {
+        }
+    }
+}
---
+++ Kerberos.Business/Concrete/AppUserManager.cs
@@ -0,0 +1,17 @@
+﻿using Kerberos.Business.Interfaces;
+using Kerberos.DataAccess.Interfaces;
+using Kerberos.Entities.Concrete;
+using Kerberos.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+namespace Kerberos.Business.Concrete
+{
+    public class AppUserManager<TEntity> : BaseManager<AppUser>, IAppUserService
+    {
+        public AppUserManager(IBaseRepository<AppUser> baseRepo) : base(baseRepo)
+        {
+        }
+    }
+}
