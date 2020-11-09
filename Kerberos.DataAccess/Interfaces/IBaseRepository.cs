735922c802768fc0cd625741beb957a26f418476
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-09 14:48:52 +0300

    implemented base repository methods

--- Kerberos.DataAccess/Interfaces/IBaseRepository.cs
+++ Kerberos.DataAccess/Interfaces/IBaseRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Kerberos.Entities.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,8 +10,8 @@ namespace Kerberos.DataAccess.Interfaces
     public interface IBaseRepository<TEntity> where TEntity : class, ITable, new()
     {
         Task<List<TEntity>> GetAll();
-        Task<List<TEntity>> GetAllByFilter(Func<TEntity,bool> filter);
-        Task<TEntity> GetByFilter(Func<TEntity, bool> filter);
+        Task<List<TEntity>> GetAllByFilter(Expression<Func<TEntity, bool>> filter);
+        Task<TEntity> GetByFilter(Expression<Func<TEntity, bool>> filter);
         Task<TEntity> GetById(int id);
         Task Update(TEntity entity);
         Task Add(TEntity entity);
---
+++ Kerberos.DataAccess/Repositories/BaseRepository.cs
@@ -0,0 +1,60 @@
+﻿using Kerberos.DataAccess.Context;
+using Kerberos.DataAccess.Interfaces;
+using Kerberos.Entities.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Kerberos.DataAccess.Repositories
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class, ITable, new()
+    {
+        public async Task Add(TEntity entity)
+        {
+            using var context = new DatabaseContext();
+            context.Add(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<List<TEntity>> GetAll()
+        {
+            using var context = new DatabaseContext();
+            return await context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<List<TEntity>> GetAllByFilter(Expression<Func<TEntity, bool>> filter)
+        {
+            using var context = new DatabaseContext();
+            return await context.Set<TEntity>().Where(filter).ToListAsync();
+        }
+
+        public async Task<TEntity> GetByFilter(Expression<Func<TEntity, bool>> filter)
+        {
+            using var context = new DatabaseContext();
+            return await context.Set<TEntity>().Where(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task<TEntity> GetById(int id)
+        {
+            using var context = new DatabaseContext();
+            return await context.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task Remove(TEntity entity)
+        {
+            using var context = new DatabaseContext();
+            context.Set<TEntity>().Remove(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Update(TEntity entity)
+        {
+            using var context = new DatabaseContext();
+            context.Set<TEntity>().Update(entity);
+            await context.SaveChangesAsync();
+        }
+    }
+}
