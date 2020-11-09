81f3969d0aec2ff91a3631daf531bcf50fbf7ec8
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-09 22:36:12 +0300

    created custom action dfilter for controlling model state is valid

---
+++ Kerberos.Util/Filters/IsValidActionFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kerberos.Util.Filters
+{
+   public class IsValidActionFilter:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var errors = context.ModelState.Values.SelectMany(a => a.Errors).Select(a => a.ErrorMessage);
+
+            if(!context.ModelState.IsValid)
+            {
+                // status code 400
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+            
+        }
+    }
+}
