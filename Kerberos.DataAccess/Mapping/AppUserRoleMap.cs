5e98e686f7bd3b4c0a1c11b594c9a19677eceb74
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-09 14:17:51 +0300

    AppUserRole map created

---
+++ Kerberos.DataAccess/Mapping/AppUserRoleMap.cs
@@ -0,0 +1,20 @@
+﻿using Kerberos.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kerberos.DataAccess.Mapping
+{
+    public class AppUserRoleMap : IEntityTypeConfiguration<AppUserRole>
+    {
+        public void Configure(EntityTypeBuilder<AppUserRole> builder)
+        {
+            builder.ToTable("tAppUserRole", "dbo");
+            builder.HasKey(a => a.Id);
+            builder.Property(a => a.Id).ValueGeneratedOnAdd();
+            builder.HasIndex(a => new {a.AppUserId, a.AppRoleId}).IsUnique(); // tekrarlı veri önlendi
+        }
+    }
+}
