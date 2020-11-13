3bf67a1c1dc55387d288ca51bb84b7eeb01395f2
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-13 16:49:48 +0300

    inserted authorize attribute

--- Kerberos.WebAPI/Controllers/ProductController.cs
+++ Kerberos.WebAPI/Controllers/ProductController.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Kerberos.Business.Interfaces;
+using Kerberos.Business.StringInfo;
+using Kerberos.DataTransferObject;
 using Kerberos.Entities.Concrete;
 using Kerberos.Util.Filters;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,25 +25,43 @@ namespace Kerberos.WebAPI.Controllers
             _productService = productService;
             _mapper = mapper;
         }
-       
+
         [HttpGet]
+        [Authorize(Roles = RoleInfo.Admin + "," + RoleInfo.Member)]
         public async Task<IActionResult> GetAllAsync()
         {
             return Ok(await _productService.GetAllAsync());
         }
         [HttpGet]
+        [Authorize(Roles = RoleInfo.Admin)]
         [ServiceFilter(typeof(IsValidIdActionFilter<Product>))]
         public async Task<IActionResult> GetByIdAsync(int id)
         {
             return Ok(await _productService.GetByIdAsync(id));
         }
+        [HttpPost]
+        [Authorize(Roles = RoleInfo.Admin)]
+        [IsValidActionFilter]
+        public async Task<IActionResult> AddProduct(ProductDto productDto)
+        {
+            await _productService.AddAsync(_mapper.Map<Product>(productDto));
+            return Created("", productDto);
+        }
+        [HttpPut]
+        [Authorize(Roles = RoleInfo.Admin)]
+        [IsValidActionFilter]
+        public async Task<IActionResult> UpdateProduct(ProductDto productDto)
+        {
+            await _productService.UpdateAsync(_mapper.Map<Product>(productDto));
+            return NoContent();
+        }
         [Route("Error")]
         public IActionResult Error()
         {
-            var errorInfo=HttpContext.Features.Get<IExceptionHandlerPathFeature>();
-           // errorInfo.Error.Message
-           // loglama
-            return Problem(detail:"An exception is occured in server side.");
+            var errorInfo = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            // errorInfo.Error.Message
+            // loglama
+            return Problem(detail: "An exception is occured in server side.");
         }
     }
 }
