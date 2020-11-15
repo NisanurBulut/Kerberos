3ea41b655cec79d2220b6ec73c05b0f53f66b28e
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-15 20:22:08 +0300

    implementing builder pattern

---
+++ Kerberos.Util/Builders/Abstract/StatusBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Kerberos.DataTransferObject;
+using Kerberos.Util.Builders.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kerberos.Util.Builders.Abstract
+{
+    // builder pattern front endt tarafta en sık kullanılan design pattern'dir
+    public abstract class StatusBuilder
+    {
+        // active user ve accepted roller bana gerekli
+        public abstract Status GenerateStatus(AppUserDto activeUser, string roles);
+    }
+}
---
+++ Kerberos.Util/Builders/Concrete/Status.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kerberos.Util.Builders.Concrete
+{
+   public class Status
+    {
+        public bool AccessStatus { get; set; }
+    }
+}
